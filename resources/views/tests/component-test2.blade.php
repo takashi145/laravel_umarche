4eb02d79e4230e43391c8f5eee3b494364b56b38
takashi145 <t.shuhei0203@gmail.com>
2022-02-19 14:59:19 +0900

    classBase

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -3,4 +3,6 @@
         ヘッダー2
     </x-slot>
     コンポーネントテスト2
+    <x-test-class-base classBaseMessage="メッセ―ジです。" />
+    <x-test-class-base classBaseMessage="メッセージです。" defaultMessage="初期値から変更しています。"/>
 </x-tests.app>
