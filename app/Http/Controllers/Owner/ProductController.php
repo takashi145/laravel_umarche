7671b2b1a7d40021f89f7a67863c2acb4625e73d
takashi145 <t.shuhei0203@gmail.com>
2022-02-22 14:48:57 +0900

    product初期設定

---
+++ app/Http/Controllers/Owner/ProductController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Owner;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
