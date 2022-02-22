faae41a9a964a785dc68a319d5ba3504c37806b3
takashi145 <t.shuhei0203@gmail.com>
2022-02-22 10:16:49 +0900

    サービスへの切り離し

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\Shop;
 use Illuminate\Support\Facades\Storage;
 use InterventionImage;
+use App\Services\ImageService;
 
 class ShopController extends Controller
 {
@@ -45,11 +46,7 @@ class ShopController extends Controller
     {
         $imageFile = $request->image;
         if(!is_null($imageFile) && $imageFile->isValid()){
-            $fileName = uniqid(rand().'_');
-            $extension = $imageFile->extension();
-            $fileNameToStore = $fileName. '.' . $extension;
-            $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
-            Storage::put('public/shops/' . $fileNameToStore, $resizedImage);
+            $fileNameToStore = ImageService::upload($imageFile, 'shops');
         }
 
         return redirect()->route('owner.shops.index');
