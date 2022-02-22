436b54392b0f73e2f9c10c106fd0b68e37db47e5
takashi145 <t.shuhei0203@gmail.com>
2022-02-22 11:12:30 +0900

    shop edit,update

--- resources/views/owner/shops/index.blade.php
+++ resources/views/owner/shops/index.blade.php
@@ -9,6 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+                    <x-flash-message status="session('status')" />
                     @foreach($shops as $shop)
                     <div class="w-1/2">
                         <a href="{{ route('owner.shops.edit', ['shop' => $shop->id]) }}">
@@ -20,13 +21,7 @@
                                         <span class="border p-2 rounded-md bg-red-400 text-white">停止中</span>
                                     @endif
                                     <div class="text-xl mt-4">{{ $shop->name }}</div>
-                                    <div>
-                                        @if(empty($shop->filename))
-                                            <img src="{{ asset('images/no_image.jpg') }}">
-                                        @else
-                                            <img scr="{{ asset('storage/shops/' . $shop->filename) }}">
-                                        @endif
-                                    </div>
+                                    <x-shop-thumbnail :filename="$shop->filename" />
                                 </div>
                             </div>
                         </a>
