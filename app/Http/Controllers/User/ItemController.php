728fe1cf2298c4cbb16221688a3c8740b924a775
takashi145 <t.shuhei0203@gmail.com>
2022-02-24 16:52:43 +0900

    商品詳細の在庫情報

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use Illuminate\Support\Facades\DB;
+use App\Models\Stock;
 
 class ItemController extends Controller
 {
@@ -42,8 +43,15 @@ class ItemController extends Controller
     }
 
     public function show($id){
-        $product = Product::findOrFail($id);
 
-        return view('user.show', compact('product'));
+        $product = Product::findOrFail($id);
+        $quantity = Stock::where('product_id', $product->id)
+        ->sum('quantity');
+
+        if($quantity > 9){
+            $quantity = 9;
+        }
+
+        return view('user.show', compact('product', 'quantity'));
     }
 }
