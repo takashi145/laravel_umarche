f85cb8079d274d8e4a704a6c78df07e1e2f7f416
takashi145 <t.shuhei0203@gmail.com>
2022-02-25 16:27:43 +0900

    テストメール

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -8,6 +8,8 @@ use App\Models\Product;
 use Illuminate\Support\Facades\DB;
 use App\Models\Stock;
 use App\Models\PrimaryCategory;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\TestMail;
 
 class ItemController extends Controller
 {
@@ -27,6 +29,8 @@ class ItemController extends Controller
         });
     }
     public function index(Request $request){
+        Mail::to('test@example.com')
+        ->send(new TestMail());
         $categories = PrimaryCategory::with('secondary')
         ->get();
 
