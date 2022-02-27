ff70cc403d9cb96156c0c4b4b246ec6bc674679a
takashi145 <t.shuhei0203@gmail.com>
2022-02-27 21:44:31 +0900

    キュー

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -10,6 +10,7 @@ use App\Models\Stock;
 use App\Models\PrimaryCategory;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\TestMail;
+use App\Jobs\SendThanksMail;
 
 class ItemController extends Controller
 {
@@ -29,8 +30,10 @@ class ItemController extends Controller
         });
     }
     public function index(Request $request){
-        Mail::to('test@example.com')
-        ->send(new TestMail());
+
+        //非同期に送信
+        SendThanksMail::dispatch();
+
         $categories = PrimaryCategory::with('secondary')
         ->get();
 
